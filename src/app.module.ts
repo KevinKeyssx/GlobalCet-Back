f6dcac727a3b8f06fca66be212ead2f8b80ec252
kevinkeyssx <keyss1700@gmail.com>
2026-05-10 01:57:06 -0400

    app module modules implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from '@app/app.controller';
+import { AppController }        from '@app/app.controller';
+import { ProductsModule }       from '@products/products.module';
+import { KitsModule }           from '@kits/kits.module';
+import { MobileLabsModule }     from '@mobile-labs/mobile-labs.module';
+import { CategoriesModule }     from '@categories/categories.module';
+import { PrismaModule }         from '@prisma/prisma.module';
+import { SubCategoriesModule }  from '@sub-categories/sub-categories.module';
 
 
 @Module({
-    imports     : [],
+    imports     : [
+        ProductsModule,
+        KitsModule,
+        MobileLabsModule,
+        CategoriesModule,
+        PrismaModule,
+        SubCategoriesModule
+    ],
     controllers : [ AppController ],
 })
 export class AppModule {}
