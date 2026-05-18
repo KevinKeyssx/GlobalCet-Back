73fd3bb7fd203f588246b8da1df315e46bfb98d8
kevinkeyssx <keyss1700@gmail.com>
2026-05-18 00:46:10 -0400

    uploadImages & deleteImage & deleteImages implemented & create updated

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,9 +6,15 @@ import {
 	Patch,
 	Param,
 	Delete,
-	Query
-}                   from '@nestjs/common';
-import { Product }  from '@prisma/client';
+	Query,
+    UseInterceptors,
+    BadRequestException,
+    UploadedFiles
+}                               from '@nestjs/common';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { FilesInterceptor }     from '@nestjs/platform-express';
+
+import { Product } from '@prisma/client';
 
 import { PaginatedResult }              from '@common/interfaces/paginated-result.interface';
 import { ProductsService }              from '@products/products.service';
@@ -16,6 +22,10 @@ import { CreateProductDto }             from '@products/dto/create-product.dto';
 import { UpdateProductDto }             from '@products/dto/update-product.dto';
 import { ProductPaginationFilterDto }   from '@products/dto/pagination-filter.dto';
 import { IProduct }                     from '@products/models/product.interface';
+import { ENVS }                         from '@config/envs';
+import { UploadProductImagesDto }       from '@products/dto/upload-product-images.dto';
+import { UpdateProductImagesDto }       from '@products/dto/update-product-images.dto';
+import { DeleteProductImagesDto }       from '@products/dto/delete-product-images.dto';
 
 
 @Controller( 'products' )
@@ -27,11 +37,49 @@ export class ProductsController {
 
 
 	@Post()
+    @ApiConsumes( 'multipart/form-data' )
+    @ApiBody({ type : CreateProductDto })
+    @UseInterceptors( FilesInterceptor( 'files', ENVS.FILE_UPLOAD_LIMIT ) )
 	create(
-		@Body() createProductDto: CreateProductDto
+		@Body() createProductDto: CreateProductDto,
+        @UploadedFiles() files   : Express.Multer.File[],
 	): Promise<IProduct> {
-		return this.productsService.create( createProductDto );
+        if ( files.length > ENVS.FILE_UPLOAD_LIMIT ) {
+            throw new BadRequestException( `You can upload a maximum of ${ ENVS.FILE_UPLOAD_LIMIT } files` );
+        }
+
+        return this.productsService.create( createProductDto, files );
 	}
+
+
+    @Post( ':id/images' )
+    @ApiConsumes( 'multipart/form-data' )
+    @UseInterceptors( FilesInterceptor( 'files', ENVS.FILE_UPLOAD_LIMIT ) )
+    uploadImages(
+        @Param( 'id' ) productId : string,
+        @UploadedFiles() files   : Express.Multer.File[],
+        @Body() uploadProductImagesDto: UploadProductImagesDto,
+    ): Promise<IProduct> {
+        if ( !files || files.length === 0 ) {
+            throw new BadRequestException( 'No se proporcionaron archivos para subir' );
+        }
+
+        if ( files.length > ENVS.FILE_UPLOAD_LIMIT ) {
+            throw new BadRequestException( `You can upload a maximum of ${ ENVS.FILE_UPLOAD_LIMIT } files` );
+        }
+
+        return this.productsService.uploadProductImages( productId, files, uploadProductImagesDto );
+    }
+
+
+    @Patch( ':id/images/info' )
+    @ApiBody({ type : UpdateProductImagesDto })
+    updateProductImagesInfo(
+        @Param( 'id' ) productId : string,
+        @Body() updateProductImagesDto : UpdateProductImagesDto,
+    ): Promise<IProduct> {
+        return this.productsService.updateProductImagesInfo( productId, updateProductImagesDto );
+    }
 
 
 	@Get()
@@ -72,5 +120,24 @@ export class ProductsController {
 	): Promise<Product> {
 		return this.productsService.remove( id );
 	}
+
+
+    @Delete( ':id/image/:imageId' )
+    deleteImage(
+        @Param( 'id' ) productId : string,
+        @Param( 'imageId' ) imageId : string,
+    ): Promise<{ message : string }> {
+        return this.productsService.deleteProductImage( productId, imageId );
+    }
+
+
+    @Post( ':id/images/delete' )
+    @ApiBody({ type : DeleteProductImagesDto })
+    deleteImages(
+        @Param( 'id' ) productId : string,
+        @Body() deleteProductImagesDto : DeleteProductImagesDto,
+    ): Promise<{ message : string }> {
+        return this.productsService.deleteProductImages( productId, deleteProductImagesDto );
+    }
 
 }
