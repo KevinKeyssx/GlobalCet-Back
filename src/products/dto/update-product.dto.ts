5231f209686a21bae32218ad860c5756a1ba4f9e
kevinkeyssx <keyss1700@gmail.com>
2026-05-22 01:27:40 -0400

    active implemented

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,17 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 
-import { CreateProductDto } from './create-product.dto';
+import { IsBoolean, IsOptional }    from 'class-validator';
+import { Transform }                from 'class-transformer';
+
+import { CreateProductDto } from '@products/dto/create-product.dto';
 
 
-export class UpdateProductDto extends PartialType( CreateProductDto ) {}
+export class UpdateProductDto extends PartialType( CreateProductDto ) {
+
+    @ApiPropertyOptional( { description: 'Filtrar activos/inactivos' } )
+	@IsOptional()
+	@IsBoolean()
+	@Transform( ({ value }) => value === 'true' || value === true )
+	active?: boolean;
+
+}
