af95f40225ebc33cb467344020325299bb55fd38
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 00:18:23 -0400

    Comments removed

--- src/products/dto/pagination-filter.dto.ts
+++ src/products/dto/pagination-filter.dto.ts
@@ -18,27 +18,6 @@ export class ProductPaginationFilterDto extends IntersectionType(
     ProductFieldsFilterDto
 ) {
 
-	// @ApiPropertyOptional({ description: 'Buscar por nombre (parcial)' })
-	// @IsOptional()
-	// @IsString()
-	// name?: string;
-
-	// @ApiPropertyOptional({ description: 'Buscar por SKU (parcial inteligente)' })
-	// @IsOptional()
-	// @IsString()
-	// sku?: string;
-
-	// @ApiPropertyOptional({ description: 'Buscar por material (parcial)' })
-	// @IsOptional()
-	// @IsString()
-	// material?: string;
-
-	// @ApiPropertyOptional({ description: 'Filtrar activos/inactivos' })
-	// @IsOptional()
-	// @IsBoolean()
-	// @Transform( ({ value }) => value === 'true' || value === true )
-	// active?: boolean;
-
 	@ApiPropertyOptional({ description: 'Listado de IDs de subcategorías', isArray: true })
 	@IsOptional()
 	@IsArray()
