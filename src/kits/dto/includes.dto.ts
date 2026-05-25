064f7f022f11993b24ce7c29204b1aefe1fe24d6
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 23:15:02 -0400

    IncludesKitDto added

---
+++ src/kits/dto/includes.dto.ts
@@ -0,0 +1,29 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+import { IsOptional, IsBoolean }    from 'class-validator';
+import { Transform }                from 'class-transformer';
+
+
+export class IncludesKitDto {
+
+    @ApiPropertyOptional( {
+		description : 'Incluir archivos asociados al kit',
+		example     : false,
+		default     : false,
+	} )
+	@IsOptional()
+	@IsBoolean()
+	@Transform( ( { value } ) => value === 'true' || value === true )
+	includeFiles?: boolean = false;
+
+	@ApiPropertyOptional( {
+		description : 'Incluir productos asociados al kit',
+		example     : false,
+		default     : false,
+	} )
+	@IsOptional()
+	@IsBoolean()
+	@Transform( ( { value } ) => value === 'true' || value === true )
+	includeProducts?: boolean = false;
+
+}
