57e486af8d9ddaadef4bb99485fb3558ee3b7ff1
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 23:10:17 -0400

    GlobalSearchQueryDto updated

--- src/global-searches/dto/global-search-query.dto.ts
+++ src/global-searches/dto/global-search-query.dto.ts
@@ -1,28 +1,82 @@
 import {
-    IsOptional,
-    IsString,
-    IsInt,
-    Min,
-    IsBoolean
-}                          from 'class-validator';
-import { Type, Transform } from 'class-transformer';
+	IsOptional,
+	IsString,
+	IsInt,
+	Min,
+	IsBoolean,
+	IsEnum
+}                              from 'class-validator';
+import { Type, Transform }     from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+
+export enum GlobalSearchSortBy {
+	CREATED_AT = 'createdAt',
+	NAME       = 'name',
+}
+
+
+export enum GlobalSearchSortOrder {
+	ASC  = 'asc',
+	DESC = 'desc',
+}
 
 
 export class GlobalSearchQueryDto {
 
+	@ApiPropertyOptional( {
+		description : 'Término de búsqueda',
+		required    : false,
+		type        : 'string',
+		example     : 'Silla',
+	} )
 	@IsOptional()
 	@IsString()
-	query?: string;
+	query? : string;
 
+	@ApiPropertyOptional( {
+		description : 'Número de resultados por entidad',
+		required    : false,
+		type        : 'number',
+		example     : 10,
+		default     : 10,
+	} )
 	@IsOptional()
 	@Type( () => Number )
 	@IsInt()
 	@Min( 1 )
-	limitPerEntity?: number = 10;
+	limitPerEntity? : number = 10;
 
+	@ApiPropertyOptional( {
+		description : 'Indica si se debe sugerir la búsqueda',
+		required    : false,
+		type        : 'boolean',
+		example     : false,
+		default     : true,
+	} )
 	@IsOptional()
 	@Transform( ( { value } ) => value === 'true' || value === true )
 	@IsBoolean()
-	suggestion?: boolean = true;
+	suggestion? : boolean = true;
+
+	@ApiPropertyOptional( {
+		description : 'Campo por el cual ordenar los resultados',
+		required    : false,
+		enum        : GlobalSearchSortBy,
+		default     : GlobalSearchSortBy.CREATED_AT,
+	} )
+	@IsOptional()
+	@IsEnum( GlobalSearchSortBy )
+	sortBy? : GlobalSearchSortBy = GlobalSearchSortBy.CREATED_AT;
+
+	@ApiPropertyOptional( {
+		description : 'Orden de clasificación',
+		required    : false,
+		enum        : GlobalSearchSortOrder,
+		default     : GlobalSearchSortOrder.ASC,
+	} )
+	@IsOptional()
+	@IsEnum( GlobalSearchSortOrder )
+	sortOrder? : GlobalSearchSortOrder = GlobalSearchSortOrder.ASC;
 
 }
