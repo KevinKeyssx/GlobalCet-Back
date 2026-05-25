a018e22f07183c025a135a3256bdb560d77b5f9c
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 23:08:22 -0400

    sortBy & sortOrder implemented

--- src/global-searches/global-searches.controller.ts
+++ src/global-searches/global-searches.controller.ts
@@ -7,52 +7,70 @@ import {
 	ApiTags
 }                                            from '@nestjs/swagger';
 
-import { GlobalSearchesService }  from './global-searches.service';
-import { GlobalSearchQueryDto }   from './dto/global-search-query.dto';
-import { IGlobalSearchResponse }  from './interfaces/global-search-result.interface';
-import { SecretGuard }            from '@common/guards/secret.guard';
+import { GlobalSearchesService }             from './global-searches.service';
+import {
+	GlobalSearchQueryDto,
+	GlobalSearchSortBy,
+	GlobalSearchSortOrder
+}                                            from './dto/global-search-query.dto';
+import { IGlobalSearchResponse }             from './interfaces/global-search-result.interface';
+import { SecretGuard }                       from '@common/guards/secret.guard';
 
 
 @ApiTags( 'GlobalSearches' )
 @UseGuards( SecretGuard )
 @Controller( 'global-searches' )
-@ApiHeader({
-    name : 'x-secret',
-    description : 'Secret key to authenticate requests',
-    required : true
-})
+@ApiHeader( {
+	name        : 'x-secret',
+	description : 'Secret key to authenticate requests',
+	required    : true,
+} )
 export class GlobalSearchesController {
 
 	constructor(
-        private readonly globalSearchesService: GlobalSearchesService
-    ) {}
+		private readonly globalSearchesService: GlobalSearchesService
+	) {}
 
 
 	@Get()
 	@ApiOperation( { summary : 'Realizar búsqueda universal inteligente sobre productos, kits y laboratorios' } )
 	@ApiResponse( { status : 200, description : 'Búsqueda ejecutada exitosamente.' } )
 	@ApiResponse( { status : 401, description : 'No autorizado: Header x-secret inválido o ausente.' } )
-    @ApiQuery( {
-        name : 'query',
-        description : 'Término de búsqueda',
-        required : true,
-        type : 'string',
-        example : 'Silla'
-    })
-    @ApiQuery( {
-        name : 'limitPerEntity',
-        description : 'Número de resultados por entidad',
-        required : false,
-        type : 'number',
-        example : 10
-    })
-    @ApiQuery( {
-        name : 'suggestion',
-        description : 'Indica si se debe sugerir la búsqueda',
-        required : false,
-        type : 'boolean',
-        example : false
-    })
+	@ApiQuery( {
+		name        : 'query',
+		description : 'Término de búsqueda',
+		required    : false,
+		type        : 'string',
+		example     : 'Silla',
+	} )
+	@ApiQuery( {
+		name        : 'limitPerEntity',
+		description : 'Número de resultados por entidad',
+		required    : false,
+		type        : 'number',
+		example     : 10,
+	} )
+	@ApiQuery( {
+		name        : 'suggestion',
+		description : 'Indica si se debe sugerir la búsqueda',
+		required    : false,
+		type        : 'boolean',
+		example     : false,
+	} )
+	@ApiQuery( {
+		name        : 'sortBy',
+		description : 'Campo por el cual ordenar los resultados',
+		required    : false,
+		enum        : GlobalSearchSortBy,
+		example     : GlobalSearchSortBy.CREATED_AT,
+	} )
+	@ApiQuery( {
+		name        : 'sortOrder',
+		description : 'Orden de clasificación (asc o desc)',
+		required    : false,
+		enum        : GlobalSearchSortOrder,
+		example     : GlobalSearchSortOrder.ASC,
+	} )
 	search(
 		@Query() queryDto: GlobalSearchQueryDto
 	): Promise<IGlobalSearchResponse> {
