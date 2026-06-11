4c4e9907009312c8460e3f5a6d52133b14768114
kevinkeyssx <keyss1700@gmail.com>
2026-06-10 22:13:57 -0400

    BaseCategoryDto extended

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,22 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import {
-    IsNotEmpty,
-    IsString,
-    Length
-} from 'class-validator';
+import { BaseCategoryDto } from '@common/dto/base-category.dto';
 
 
-export class CreateCategoryDto {
-
-    @ApiProperty( {
-        description : 'Category name',
-        example     : 'Electronics',
-    } )
-    @IsString()
-    @IsNotEmpty()
-    @Length( 1, 200, {
-        message : 'name must be between 1 and 200 characters',
-    })
-    name: string;
-
-}
+export class CreateCategoryDto extends BaseCategoryDto {}
