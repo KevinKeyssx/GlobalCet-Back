c364942b881016a9ee7a1967148ba30972e49b6c
kevinkeyssx <keyss1700@gmail.com>
2026-06-16 17:33:04 -0400

    matchFileByName implemented

--- src/common/utils/file.utils.ts
+++ src/common/utils/file.utils.ts
@@ -6,6 +6,18 @@ export const getFileNameWithExtension = ( secureUrl: string ): string => {
 };
 
 
+export const matchFileByName = ( serverUrlOrName: string, clientName: string ): boolean => {
+	if ( !clientName ) return false;
+
+	const serverNameLower = decodeURIComponent( serverUrlOrName.toLowerCase() );
+	const clientNameLower = clientName.toLowerCase();
+	const serverBase      = serverNameLower.substring( 0, serverNameLower.lastIndexOf( '.' ) ) || serverNameLower;
+	const clientBase      = clientNameLower.substring( 0, clientNameLower.lastIndexOf( '.' ) ) || clientNameLower;
+
+	return serverBase.includes( clientBase ) || clientBase.includes( serverBase );
+};
+
+
 export const mapResourceTypeToAttachmentType = ( resourceType: string, secureUrl: string ) : AttachmentType => {
 	const filename = secureUrl.toLowerCase();
 
