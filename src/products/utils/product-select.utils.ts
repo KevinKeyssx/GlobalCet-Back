3627f408b00bed17e8ba05a4210fd908d16de66a
kevinkeyssx <keyss1700@gmail.com>
2026-06-16 19:42:21 -0400

    getProductSelect added

---
+++ src/products/utils/product-select.utils.ts
@@ -0,0 +1,70 @@
+import { Prisma } from '@prisma/client';
+
+
+export function getProductSelect(
+	includeFiles		: boolean = false,
+	includeKits			: boolean = false,
+	includeMobileLabs	: boolean = false
+): Prisma.ProductSelect {
+	return {
+		id				: true,
+		sku				: true,
+		name			: true,
+		description		: true,
+		material		: true,
+		technical_specs	: true,
+		active			: true,
+		createdAt		: true,
+		updatedAt		: true,
+		subcategory		: {
+			select : {
+				id		: true,
+				name	: true
+			}
+		},
+		files			: {
+			where	: includeFiles ? {} : { isMain : true },
+			select	: {
+				id				: true,
+				url				: true,
+				alt				: true,
+				isMain			: true,
+				order			: true,
+				attachmentType	: true
+			}
+		},
+		...( includeKits && {
+			inKits : {
+				select : {
+					id			: true,
+					quantity	: true,
+					kit			: {
+						select : {
+							id			: true,
+							sku			: true,
+							name		: true,
+							description	: true
+						}
+					}
+				}
+			}
+		} ),
+		...( includeMobileLabs && {
+			inMobileLabs : {
+				select : {
+					id			: true,
+					quantity	: true,
+					mobileLab	: {
+						select : {
+							id			: true,
+							sku			: true,
+							name		: true,
+							description	: true,
+							dimensions	: true
+						}
+					}
+				}
+			}
+		} )
+	};
+}
