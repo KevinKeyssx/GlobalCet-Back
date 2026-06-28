eb0821e514e43d1786f6a8ca7d6be3b23caef014
kevinkeyssx <keyss1700@gmail.com>
2026-06-28 16:48:43 -0400

    CreatePriceHistoryService implemented

--- src/kits/kits.module.ts
+++ src/kits/kits.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { FileManagerService }   from '@services/file-manager.service';
-import { KitsController }       from './kits.controller';
-import { KitsService }          from './kits.service';
+import { FileManagerService }         from '@services/file-manager.service';
+import { CreatePriceHistoryService }  from '@common/service/create-price-history.service';
+import { KitsController }             from './kits.controller';
+import { KitsService }                from './kits.service';
 
 
 @Module( {
 	controllers : [ KitsController ],
-	providers   : [ KitsService, FileManagerService ],
+	providers   : [ KitsService, FileManagerService, CreatePriceHistoryService ],
 } )
 export class KitsModule {}
+
