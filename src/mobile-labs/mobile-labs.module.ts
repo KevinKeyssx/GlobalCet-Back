b3647cc01c36491b821437daba4abd55187bd696
kevinkeyssx <keyss1700@gmail.com>
2026-06-28 16:56:27 -0400

    CreatePriceHistoryService implemented

--- src/mobile-labs/mobile-labs.module.ts
+++ src/mobile-labs/mobile-labs.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { FileManagerService }   from '@services/file-manager.service';
-import { MobileLabsController } from './mobile-labs.controller';
-import { MobileLabsService }    from './mobile-labs.service';
+import { FileManagerService }         from '@services/file-manager.service';
+import { CreatePriceHistoryService }  from '@common/service/create-price-history.service';
+import { MobileLabsController }       from './mobile-labs.controller';
+import { MobileLabsService }          from './mobile-labs.service';
 
 
 @Module( {
 	controllers : [ MobileLabsController ],
-	providers   : [ MobileLabsService, FileManagerService ],
+	providers   : [ MobileLabsService, FileManagerService, CreatePriceHistoryService ],
 } )
 export class MobileLabsModule {}
+
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
-import { FileManagerService }   from '@services/file-manager.service';
-import { ProductsController }   from '@products/products.controller';
-import { ProductsService }      from '@products/products.service';
+import { FileManagerService }         from '@services/file-manager.service';
+import { ProductsController }         from '@products/products.controller';
+import { ProductsService }            from '@products/products.service';
+import { CreatePriceHistoryService }  from '@common/service/create-price-history.service';
 
 
 @Module({
     controllers : [ ProductsController ],
-    providers   : [ ProductsService, FileManagerService ],
+    providers   : [ ProductsService, FileManagerService, CreatePriceHistoryService ],
 })
 export class ProductsModule {}
